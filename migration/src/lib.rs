06c9ee26f8cd6996c27818315f87c2f669edb65d
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-01-10 19:48:48 +0800

    feat: roles table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20260109_154851_create_table_users;
+mod m20260110_113127_create_table_roles;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20260109_154851_create_table_users::Migration)]
+        vec![
+            Box::new(m20260109_154851_create_table_users::Migration),
+            Box::new(m20260110_113127_create_table_roles::Migration),
+        ]
     }
 }
---
+++ migration/src/m20260110_113127_create_table_roles.rs
@@ -0,0 +1,33 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+const TABLE_NAME: &str = "roles";
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(TABLE_NAME)
+                    .if_not_exists()
+                    .col(pk_auto("id"))
+                    .col(string_uniq("name"))
+                    .col(string_null("description"))
+                    .col(boolean("is_system_role").default(false)) // cannot be deleted
+                    .col(integer("priority").default(0)) // higher number = higher priority
+                    .col(timestamp_with_time_zone("created_at").default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone("updated_at").default(Expr::current_timestamp()))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(TABLE_NAME).to_owned())
+            .await
+    }
+}
