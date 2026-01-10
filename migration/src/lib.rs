ca8398974af4c99789a5ce061ce405de5c797783
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-01-10 19:20:19 +0800

    feat: core user table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,10 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
+mod m20260109_154851_create_table_users;
+
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
+        vec![Box::new(m20260109_154851_create_table_users::Migration)]
     }
 }
---
+++ migration/src/m20260109_154851_create_table_users.rs
@@ -0,0 +1,65 @@
+use sea_orm_migration::{
+    prelude::{extension::postgres::Type, *},
+    schema::*,
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+const TABLE_NAME: &str = "users";
+const ENUM_NAME: &str = "user_status";
+const ENUM_VALUES: [&str; 3] = ["inactive", "active", "banned"];
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_type(
+                Type::create()
+                    .as_enum(ENUM_NAME)
+                    .values(ENUM_VALUES)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(TABLE_NAME)
+                    .if_not_exists()
+                    .col(pk_uuid("id").default(Expr::cust("uuidv7()")))
+                    // Credentials
+                    .col(string_uniq("username"))
+                    .col(string_uniq("email"))
+                    .col(string("password_hash")) // argon2 hash
+                    // 2FA
+                    .col(boolean("totp_enabled").default(false))
+                    .col(string_null("totp_secret_enc")) // base32 encoded wiht aes
+                    .col(json_binary_null("recover_codes_enc")) // aes encrypted array
+                    // Refresh Session Management
+                    .col(integer("token_version")) // for mass revocation
+                    // Account State
+                    .col(boolean("email_verified").default(false))
+                    .col(enumeration("status", ENUM_NAME, ENUM_VALUES).default(ENUM_VALUES[0]))
+                    // Timestamps
+                    .col(timestamp_with_time_zone("created_at").default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone("updated_at").default(Expr::current_timestamp()))
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(TABLE_NAME).to_owned())
+            .await?;
+
+        manager
+            .drop_type(Type::drop().name(ENUM_NAME).to_owned())
+            .await?;
+
+        Ok(())
+    }
+}
