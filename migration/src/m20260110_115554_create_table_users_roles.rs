9d61f54a14b547269ab625b94f6a6647530e32a0
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-01-10 20:29:23 +0800

    feat: users_roles table

---
+++ migration/src/m20260110_115554_create_table_users_roles.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+const TABLE_NAME: &str = "users_roles";
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(TABLE_NAME)
+                    .if_not_exists()
+                    .col(ColumnDef::new("user_id").uuid().not_null().primary_key())
+                    .col(ColumnDef::new("role_id").integer().not_null().primary_key())
+                    .col(timestamp_with_time_zone("assigned_at").default(Expr::current_timestamp()))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_user_id")
+                            .from(TABLE_NAME, "user_id")
+                            .to("users", "id")
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_role_id")
+                            .from(TABLE_NAME, "role_id")
+                            .to("roles", "id")
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(TABLE_NAME).to_owned())
+            .await
+    }
+}
